73483e88a1769290a54e8e6f14e408823d432eeb
nazmul910 <islamnasir910@gmail.com>
2026-05-02 01:44:42 +0600

    feat: implement UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(private prisma: PrismaService){}
+
+    async getUserById(id:string){
+        return this.prisma.user.findUnique({
+            where:{id},
+        });
+    }
+
+    async getUserByTenant(tenantId:string){
+        return this.prisma.user.findMany({
+            where:{tenantId},
+        })
+    }
+}
