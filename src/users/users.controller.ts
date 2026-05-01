dee3e8a6b1687be713fba1e3574f02884e0554eb
nazmul910 <islamnasir910@gmail.com>
2026-05-02 01:48:49 +0600

    feat: implement UsersController with me and all endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Headers } from '@nestjs/common';
+import { UsersService } from './users.service';
+import * as jwt from 'jsonwebtoken';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private usersService: UsersService){}
+
+    @Get('me')
+    async me(@Headers('authorization') auth:string){
+        const token = auth.split(' ')[1];
+        const deconded:any = jwt.verify(token,"SECRET_KEY");
+
+        return this.usersService.getUserById(deconded.userId);
+    }
+
+    @Get()
+    async all(@Headers('authorization') auth:string){
+        const token = auth.split(' ')[1];
+        const deconded:any = jwt.verify(token,"SECRET_KEY");
+
+        return this.usersService.getUserByTenant(deconded.tenantId);
+    }
+}
