db1ccd7affb36dd9e81c30e1c3147b6c9110d18f
nazmul910 <islamnasir910@gmail.com>
2026-05-02 00:48:09 +0600

    feat: implement initial module structure with Auth, Users, Payment, and Prisma modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,27 +1,10 @@
-import { Injectable, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from "generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
-import * as dotenv from "dotenv";
 
-dotenv.config();
+import { Global, Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
-@Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    // 1. Create PostgreSQL connection pool
-    const pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
-    });
-
-    // 2. Pass the pool to PrismaPg adapter
-    const adapter = new PrismaPg(pool);
-
-    // 3. Initialize PrismaClient with adapter
-    super({ adapter });
-  }
-
-  async onModuleInit() {
-    await this.$connect();
-  }
-}
+@Global() // OPTIONAL but recommended
+@Module({
+  providers: [PrismaService],
+  exports: [PrismaService],
+})
+export class PrismaModule {}
