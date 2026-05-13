6a3a57ceba91684e2651a0b424f4547cbaf41dd9
nazmul910 <islamnasir910@gmail.com>
2026-05-14 00:13:40 +0600

    fix: update gatewayURL to use localhost for payment success

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { v4 as uuid } from 'uuid';
 import { PlanType } from 'generated/prisma/enums';
+import axios from 'axios';
 
 @Injectable()
 export class PaymentService {
@@ -9,24 +10,60 @@ export class PaymentService {
     private prisma: PrismaService,
   ) {}
 
-  createPayment(userId: string, amount: number) {
+async  createPayment(userId: string, amount: number) {
     const tran_id = uuid();
 
-    return {
-      gatewayURL: process.env.SSL_URL,
-      payload: {
-        store_id: process.env.STORE_ID,
-        store_pass: process.env.STORE_PASS,
-        total_amount: amount,
-        currency: 'BDT',
-        tran_id,
-        success_url: 'http://localhost:3000/payment/success',
-        fail_url: 'http://localhost:3000/payment/fail',
-        cancel_url: 'http://localhost:3000/payment/cancel',
-        value_a: userId,
-      },
+const paymentData = {
+      store_id: process.env.STORE_ID,
+      store_passwd: process.env.STORE_PASS,   
+      total_amount: amount,
+      currency: 'BDT',
+      tran_id,
+
+      success_url: 'http://localhost:5002/api/payment/success',
+      fail_url: 'http://localhost:5002/api/payment/fail',
+      cancel_url: 'http://localhost:5002/api/payment/cancel',
+
+      cus_name: 'Customer',
+      cus_email: 'customer@example.com',
+      cus_phone: '01700000000',
+      cus_add1: 'Dhaka',
+      cus_city: 'Dhaka',
+      cus_country: 'Bangladesh',
+
+      shipping_method: 'NO',
+      product_name: 'Subscription Plan',
+      product_category: 'General',
+      product_profile: 'general',
+
+      value_a: userId,
     };
-  }
+
+    try {
+      const response = await axios.post(
+        `${process.env.SSL_URL}/gwprocess/v4/api.php`,
+        new URLSearchParams(paymentData as any).toString(),
+        {
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        },
+      );
+
+      const gatewayURL = response.data.GatewayPageURL;
+
+      if (!gatewayURL) {
+        console.error('SSLCommerz response:', response.data);
+        throw new InternalServerErrorException('Gateway URL not received');
+      }
+
+      return { gatewayURL };  
+    } catch (error) {
+      console.error('Error creating payment:', error);
+      throw new InternalServerErrorException('Failed to create payment');
+    }
+    }
+
+
+  
 
 
 async paymentSuccess(body: any) {
@@ -63,7 +100,6 @@ async paymentSuccess(body: any) {
 
   return { message: 'Payment success' };
 }
-
 
 
 }
