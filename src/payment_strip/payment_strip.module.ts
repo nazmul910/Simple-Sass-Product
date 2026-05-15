ad61538bda0861433c6767c5d11392ec6f39df1f
nazmul910 <islamnasir910@gmail.com>
2026-05-15 23:04:42 +0600

    feat: configure PaymentStrip module with providers and controllers

--- src/payment_strip/payment_strip.module.ts
+++ src/payment_strip/payment_strip.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PaymentStripController } from './payment_strip.controller';
+import { PaymentStripService } from './payment_strip.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
-@Module({})
+@Module({
+    providers:[PaymentStripService,PrismaService],
+    controllers:[PaymentStripController]
+})
 export class PaymentStripModule {}
