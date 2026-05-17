2e89c1f0b3947b1224062ed2fdcbd389dee0a1ca
nazmul910 <islamnasir910@gmail.com>
2026-05-17 13:20:38 +0600

    feat: enhance PaymentStripController with payment creation and webhook handling

--- src/payment_strip/payment_strip.controller.ts
+++ src/payment_strip/payment_strip.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Headers, Post, Req, Res } from '@nestjs/common';
+import { PaymentStripService } from './payment_strip.service';
+import type { Request, Response } from 'express';
 
 @Controller('payment-strip')
-export class PaymentStripController {}
+export class PaymentStripController {
+    constructor(private paymentStripService: PaymentStripService){}
+
+
+    @Post()
+    async create(@Body() body:any){
+        return this.paymentStripService.createPayment(body.userId, body.amount);
+    }
+
+    @Post('webhook')
+    async webhook(
+        @Req() req: Request & { rawBody: Buffer },
+        @Headers('stripe-signature') signature: string,
+        @Res() res: Response,
+    ){
+        try {
+            const result = await this.paymentStripService.handleWebhook(
+                req.rawBody,
+                signature,
+            );
+            res.status(200).send('Webhook received');
+        } catch (error) {
+            console.error('Error handling webhook:', error);
+            res.status(500).send('Error handling webhook');
+        }
+    }
+}
