4f468c308ca0e8f02be44b81ec62448eba220544
nazmul910 <islamnasir910@gmail.com>
2026-05-26 01:47:25 +0600

    fix: update route descriptions for basic and standard features in FeaturesController

--- src/features/features.controller.ts
+++ src/features/features.controller.ts
@@ -8,13 +8,13 @@ export class FeaturesController {
   constructor(private featuresService: FeaturesService) {}
 
   @UseGuards(JwtGuard, new PlanGuard('BASIC'))
-  @Get('Your Basic Feature')
+  @Get('Your Basic Feature is activated with your subscription')
   feature1(@Req() req: any) {
     return this.featuresService.getFeature1(req.user.userId);
   }
 
   @UseGuards(JwtGuard, new PlanGuard('STANDARD'))
-  @Get('Your Standard Feature')
+  @Get('Your Standard Feature is activated with your subscription')
   feature2(@Req() req: any) {
     return this.featuresService.getFeature2(req.user.userId);
   }
